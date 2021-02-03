777df6c88aa8470b7444881b6d9a992c484a91c9
xzhao11 <xzhao11@calpoly.edu>
2021-02-02 16:49:40 -0800

    Basic Env Setup

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 offset;
+    // change this value to get desired smoothness
+    public float SmoothTime = 0.3f;
+    // This value will change at the runtime depending on target movement. Initialize with zero vector.
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 targetPosition = player.transform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, SmoothTime);
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Code Source: THIRD PERSON MOVEMENT in Unity
+        //https://www.youtube.com/watch?v=4HpC--2iowE&t=1002s
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(horizontal, 0.0f, vertical).normalized;
+        if (movement.magnitude >= 0)
+        {
+
+  
+
+
+            controller.Move(movement * speed * Time.deltaTime);
+
+        }
+    }
+}
