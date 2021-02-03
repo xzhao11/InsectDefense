cbac6f411352abe656cf7f9494391b4c0e406202
acatelan <anthonyjcatelani@gmail.com>
2021-02-03 15:43:13 -0800

    added basic enemy and tower look logic

---
+++ Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour
+{
+    public float _enemySpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(_enemySpeed * Time.deltaTime, 0f, 0f);
+    }
+}
