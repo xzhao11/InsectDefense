7854c15d913f2ce10ec73c44d601a93ed19e2923
acatelan <anthonyjcatelani@gmail.com>
2021-02-16 21:13:56 -0800

    simplified nest object

---
+++ Assets/Scripts/nestScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nestScript : MonoBehaviour
+{
+    public int numLarva;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numLarva = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void decreaseLarva()
+    {
+        numLarva -= 1;
+    }
+}
