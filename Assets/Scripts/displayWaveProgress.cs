0c6bbd39feb0fb0826e9c8a42b2d06345e18a608
acatelan <anthonyjcatelani@gmail.com>
2021-02-17 22:16:51 -0800

    Merge branch 'master' into acatelan-intermediate

---
+++ Assets/Scripts/displayWaveProgress.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+public class displayWaveProgress : MonoBehaviour
+{
+    public TMP_Text output;
+    public GameObject waveData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int num = waveData.GetComponent<waveScript>().numEnemies;
+        int total = waveData.GetComponent<waveScript>().totalEnemies;
+        int prog = (int)Math.Round(((double) num / (double) total) * 100);
+        output.text = "Wave Progress: " + prog + "%";
+    }
+}
---
+++ Assets/Scripts/waveScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waveScript : MonoBehaviour
+{
+    public int numEnemies = 0;
+    public int totalEnemies;
+    public float downTime = 60f;
+    public GameObject enemy;
+    private float timeLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = downTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeLeft <= 0 && numEnemies < totalEnemies)
+        {
+            Instantiate(enemy, new Vector3(-122.6f, -13.478f, 143.75f), Quaternion.identity);
+            timeLeft = downTime;
+            numEnemies += 1;
+        }
+        else
+        {
+            timeLeft -= 1;
+        }
+    }
+}
