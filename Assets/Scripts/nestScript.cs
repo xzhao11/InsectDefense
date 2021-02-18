82eca730c4a71c92eec815d363d875753589c51f
xzhao11 <xzhao11@calpoly.edu>
2021-02-17 19:50:39 -0800

    add ui for num larva

--- Assets/Scripts/nestScript.cs
+++ Assets/Scripts/nestScript.cs
@@ -1,21 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class nestScript : MonoBehaviour
 {
     public int numLarva;
+    [SerializeField] TMP_Text num_msg;
 
     // Start is called before the first frame update
     void Start()
     {
-        numLarva = 100;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        num_msg.text = "number of larva is "+ numLarva;
     }
 
     void decreaseLarva()
