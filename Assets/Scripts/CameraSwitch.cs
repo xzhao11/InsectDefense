4ef2b95b3d1618c38a7b3bf3d45c151d7aab13cf
Nathaniel Copeland <nathanielc510@gmail.com>
2021-02-23 13:04:56 -0800

    Initial Camera Switch Script Added
    To be improved

---
+++ Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public Camera thirdPerson;
+    public Camera topDown;
+    public bool thirdActive;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        thirdPerson.enabled = true;
+        topDown.enabled = false;
+        thirdActive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (thirdActive) 
+            {
+                thirdPerson.enabled = false;
+                topDown.enabled = true;
+                thirdActive = false;
+            }
+            else
+            {
+                thirdPerson.enabled = true;
+                topDown.enabled = false;
+                thirdActive = true;
+            }
+        }
+    }
+}
