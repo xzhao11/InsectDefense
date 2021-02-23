12bac76dab2d3599ef9964b60b2fd70c8d0137f8
xzhao11 <xzhao11@calpoly.edu>
2021-02-23 14:05:20 -0800

    Change direction in third person to a global

--- Assets/Scripts/PlayerThirdPersonControl.cs
+++ Assets/Scripts/PlayerThirdPersonControl.cs
@@ -14,7 +14,7 @@ public class PlayerThirdPersonControl : MonoBehaviour
 
 
     public Transform cam;
-    private
+    public Vector3 moveDir;
 
     //AudioSource step;
     void Start()
@@ -36,7 +36,7 @@ public class PlayerThirdPersonControl : MonoBehaviour
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
 
