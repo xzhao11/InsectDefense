842e0e0ef5638400fc4f52123f2055a054b17a3b
xzhao11 <xzhao11@calpoly.edu>
2021-02-28 17:03:37 -0800

    fix player movement in top down view

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,7 +5,9 @@ using Cinemachine;
 public class PlayerControl : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
-    [SerializeField] float speed;
+    [SerializeField] float speed; 
+    private float gravity;
+    [SerializeField] float groundY = 3;
 
     public float turnSmoothTime = 0.1f;
     float smooth=5.0f;
@@ -16,10 +18,19 @@ public class PlayerControl : MonoBehaviour
     {
         //Code Source: THIRD PERSON MOVEMENT in Unity
         //https://www.youtube.com/watch?v=4HpC--2iowE&t=1002s
-        float horizontal = -Input.GetAxisRaw("Horizontal");
-        float vertical = -Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
         Vector3 movement = new Vector3(horizontal, 0.0f, vertical).normalized;
-        
+        gravity -= (float)9.81 * Time.deltaTime;
+        if (transform.position.y == groundY)
+        {
+            gravity = 0.0f;
+        }
+        else
+        {
+            Vector3 down = new Vector3(0, gravity, 0);
+            controller.Move(down);
+        }
         if (movement.magnitude > 0)
         {
             var delta = -transform.position;
