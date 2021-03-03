7deb4a573f6bbe4da6c95fa4f67ada1049c8eacd
acatelan <anthonyjcatelani@gmail.com>
2021-03-02 16:17:11 -0800

    added basic multipathing

--- Assets/Scripts/enemy/tileWaveScript.cs
+++ Assets/Scripts/enemy/tileWaveScript.cs
@@ -5,24 +5,32 @@ using UnityEngine;
 public class tileWaveScript : MonoBehaviour
 {
     public GameObject[] enemies;
-    public GameObject path;
+    public GameObject[] paths;
     public int totalEnemies = 100;
     public int remaining;
     public float downTime = 1200f;
     private float timeToSpawn = 0f;
-    private Vector3 spawn;
+    private Vector3[] spawns;
     public Transform nest;
     public GameObject player;
     private int num = 0;
+    private int whichPath = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        spawns = new Vector3[paths.Length];
+
         for (int i = 0; i < enemies.Length; i++)
         {
             enemies[i].SetActive(false);
         }
-        spawn = path.transform.GetChild(0).GetChild(0).position;
+        for (int j = 0; j < paths.Length; j++)
+        {
+            Debug.Log(paths[j]);
+            spawns[j] = paths[j].transform.GetChild(0).GetChild(0).position;
+        }
+
         remaining = totalEnemies;
     }
 
@@ -32,9 +40,11 @@ public class tileWaveScript : MonoBehaviour
         if(timeToSpawn <= 0 && remaining != 0 && Time.timeScale >0)
         {
             num = num % enemies.Length;
-            var curr_enemy = (GameObject)Instantiate(enemies[num], spawn, Quaternion.identity);
+            whichPath = whichPath % paths.Length;
+
+            var curr_enemy = (GameObject)Instantiate(enemies[num], spawns[whichPath], Quaternion.identity);
             curr_enemy.SetActive(true);
-            curr_enemy.GetComponent<tileMovement>().path = path;
+            curr_enemy.GetComponent<tileMovement>().path = paths[whichPath];
             curr_enemy.GetComponent<tileMovement>().nest = nest;
             //curr_enemy.GetComponent<enemy>().nest = nest.transform;
             curr_enemy.GetComponent<enemy>().health = 20;
@@ -42,6 +52,7 @@ public class tileWaveScript : MonoBehaviour
             remaining -= 1;
             timeToSpawn = downTime;
             num++;
+            whichPath++;
         }
         else
         {
