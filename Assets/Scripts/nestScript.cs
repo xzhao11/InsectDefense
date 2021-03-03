0efe750231772326120c7cb5fcda2ac8ad1573f5
acatelan <anthonyjcatelani@gmail.com>
2021-03-02 17:16:54 -0800

    serialized twin chance and triplet chance in case we want to use it for upgrades

--- Assets/Scripts/nestScript.cs
+++ Assets/Scripts/nestScript.cs
@@ -5,6 +5,8 @@ using TMPro;
 public class nestScript : MonoBehaviour
 {
     public int numLarva;
+    public float twinChance = 0.25f;
+    public float tripletChance = 0.1f;
     [SerializeField] TMP_Text num_msg;
 
     // Start is called before the first frame update
@@ -26,6 +28,6 @@ public class nestScript : MonoBehaviour
 
     public void repopulate()
     {
-        numLarva += (int)(numLarva * 0.25) + (int)(numLarva * 0.1);
+        numLarva += (int)(numLarva * twinChance) + (int)(numLarva * tripletChance);
     }
 }
