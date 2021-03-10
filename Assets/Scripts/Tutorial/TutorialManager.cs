bd1ca68621d345cbe1b2b4efb524005801971a3b
xzhao11 <xzhao11@calpoly.edu>
2021-03-10 14:27:28 -0800

    Added first story text typing effect for tutorial

---
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour
+{
+    private float timer;
+    public bool timerIsRunning = false;
+    public Canvas tutorialCanvas;
+    public Text startStory;
+    void Start()
+    {
+        timerIsRunning = true;
+        timer = 0;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer == 0)
+        {
+            timerIsRunning = false;
+            tutorialCanvas.gameObject.SetActive(true);
+            startStory.gameObject.SetActive(true);
+            if (startStory.GetComponent<TextTyping>().isFinished)
+            {
+                timerIsRunning = true;
+            }
+        }
+
+        if (timerIsRunning)
+        {
+            timer += Time.deltaTime;
+        }
+
+
+    }
+
+}
---
+++ Assets/TextTyping.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextTyping : MonoBehaviour
+{
+    string fullText;
+	private string current = "";
+	public bool isFinished;
+	void Awake()
+	{
+		fullText = this.GetComponent<Text>().text;
+		this.GetComponent<Text>().text = "";
+		isFinished = false;
+		StartCoroutine(PlayText());
+	}
+
+	IEnumerator PlayText()
+	{
+		for(int i = 0; i < fullText.Length; i++)
+		{
+			current = fullText.Substring(0, i);
+			this.GetComponent<Text>().text = current;
+			if (current.Length == fullText.Length-1)
+            {
+				isFinished = true;
+            }
+			yield return new WaitForSeconds(0.05f);
+		}
+	}
+}
