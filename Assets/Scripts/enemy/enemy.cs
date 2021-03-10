772535b087bb7497d354c0bc8f71219dd2767989
xzhao11 <xzhao11@calpoly.edu>
2021-03-09 17:27:05 -0800

    add audio for enemy dying and an audio mixer

--- Assets/Scripts/enemy/enemy.cs
+++ Assets/Scripts/enemy/enemy.cs
@@ -13,6 +13,7 @@ public class enemy : MonoBehaviour
     public GameObject wave;
     //[SerializeField] float hitDistance = 30f;
     [SerializeField] Image healthBar;
+    [SerializeField] ParticleSystem diedEffect;
 
     private void Start()
     {
@@ -26,6 +27,7 @@ public class enemy : MonoBehaviour
             //Debug.Log("enemy down");
             //health_msg.text = "Enemy Down";
             wave.GetComponent<tileWaveScript>().decrementRemaining();
+            Instantiate(diedEffect, transform.position, Quaternion.identity);
             Destroy(gameObject);
 
         }
