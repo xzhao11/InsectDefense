cf77677c8aa61b16f5bff2d333676cc3ad43660a
xzhao11 <xzhao11@calpoly.edu>
2021-03-09 18:26:51 -0800

    select level  & resume / new game

--- Assets/Scripts/menu/menu.cs
+++ Assets/Scripts/menu/menu.cs
@@ -5,18 +5,56 @@ using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
+    int level;
+    //private void Start()
+    //{
+    //    PlayerPrefs.SetInt("Level", 3);
+    //}
     public void PlayGame()
     {
         //SceneManager.LoadScene(3);
-        StartCoroutine(DelaySceneLoad(3));
+        
+        level = PlayerPrefs.GetInt("Level");
+        if (level == 0)
+        {
+            level = 3;
+        }
+        StartCoroutine(DelaySceneLoad(level));
+        
     }
-
     public void BackToMainMenu()
     {
         //SceneManager.LoadScene(0);
         StartCoroutine(DelaySceneLoad(0));
     }
+    public void PlayLevel1()
+    {
+        //SceneManager.LoadScene(3);
+        PlayerPrefs.SetInt("Level", 3);
+        level = PlayerPrefs.GetInt("Level");
+        StartCoroutine(DelaySceneLoad(3));
+        
+    }
 
+    public void PlayLevel2()
+    {
+        //SceneManager.LoadScene(3);
+        PlayerPrefs.SetInt("Level", 4);
+        level = PlayerPrefs.GetInt("Level");
+        StartCoroutine(DelaySceneLoad(4));
+        
+    }
+
+    public void PlayLevel3()
+    {
+        //SceneManager.LoadScene(3);
+        PlayerPrefs.SetInt("Level", 4);
+        level = PlayerPrefs.GetInt("Level");
+        StartCoroutine(DelaySceneLoad(4));
+        
+    }
+
+    
     public void GoToSetting()
     {
         //SceneManager.LoadScene(1);
