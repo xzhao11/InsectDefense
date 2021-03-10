4099d4243307eeef841a2790d7b7b02357e1def5
xzhao11 <xzhao11@calpoly.edu>
2021-03-09 18:01:32 -0800

    Add sound to UI

--- Assets/Scripts/menu/menu.cs
+++ Assets/Scripts/menu/menu.cs
@@ -7,22 +7,32 @@ public class menu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(3);
+        //SceneManager.LoadScene(3);
+        StartCoroutine(DelaySceneLoad(3));
     }
 
     public void BackToMainMenu()
     {
-        SceneManager.LoadScene(0);
+        //SceneManager.LoadScene(0);
+        StartCoroutine(DelaySceneLoad(0));
     }
 
     public void GoToSetting()
     {
-        SceneManager.LoadScene(1);
+        //SceneManager.LoadScene(1);
+        StartCoroutine(DelaySceneLoad(1));
     }
 
     public void SelectLevels()
     {
-        SceneManager.LoadScene(2);
+        //SceneManager.LoadScene(2);
+        StartCoroutine(DelaySceneLoad(2));
+    }
+
+    IEnumerator DelaySceneLoad(int sceneNum)
+    {
+        yield return new WaitForSeconds(0.6f);
+        SceneManager.LoadScene(sceneNum);
     }
 }
 
