7aa6dfe873966fa4fa63ebbd7d138868b5633189
xzhao11 <xzhao11@calpoly.edu>
2021-03-09 17:43:24 -0800

    volume is saved for player

--- Assets/Scripts/setVolume.cs
+++ Assets/Scripts/setVolume.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class setVolume : MonoBehaviour
 {
     public AudioMixer mixer;
+    public Slider slider;
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+    }
     public void SetLevel(float sliderValue)
     {
         mixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
     }
 }
